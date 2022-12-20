3fd548af20e21b242ae9270496a61bc40ef69297
Ricardo Loureiro <ricardoloureiro75@gmail.com>
2022-12-20 09:24:10 -0300

    Arrumando erros do teste

--- TP1/tst/RendimentoTeste.java
+++ TP1/tst/RendimentoTeste.java
@@ -29,59 +29,26 @@ public class RendimentoTeste {
 		simulador = new SimuladorIRPF();
 	}
 	
-	public CadastroRendimentosTeste(Object[][] rendimentos, float valor) {
-		this.rendimentos = rendimentos;
-		this.valor = valor;
-	}
-	
-	@Parameters
-	public static Collection<Object[]> getParameters(){
-		Object[][] retorno = new Object[][] {
-			{new Object[][] {
-				{"Salario", 500f},
-			}, 500f},
-			{new Object[][] {
-				{"Salario", 4000f}, 
-				{"Aluguel", 10000f}
-			}, 14000f}, 
-			{new Object[][] {
-				{"Salario", 5000f}, 
-				{"Aluguel", 9000f}
-			}, 11000f}
-		};
-		
-		return Arrays.asList(retorno);
-	}
-	
-	
 	@Test
-	public void testCadastroRendimentos() {
-		for (Object[] objeto : rendimentos) {
-			simulador.cadastroRendimento((String)objeto[0], (float)objeto[1]);
-		}
-		assertEquals(valor, simulador.getTotalRendimento(), 0f);
-	}
-	
-	@Test
-	public void testeCadastraUmRendimento() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
+	public void testeCadastroRendimento() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
 		simulador.cadastroRendimento("Salario", 10000f);
 		assertEquals(10000f, simulador.getTotalRendimento(), 0f);
 	}
 	
 	@Test(expected = DescricaoEmBrancoException.class)
-	public void testeCadastraRendimentoDescricaoEmBranco() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
+	public void testeCadastroRendimentoDescricaoEmBranco() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
 		simulador.cadastroRendimento(null, 10000f);
 		assertEquals(10000f, simulador.getTotalRendimento(), 0f);
 	}
 	
 	@Test (expected = ValorRendimentoInvalidoException.class)
-	public void testeCadastraRendimentoValorZero() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
+	public void testeCadastroRendimentoValorZero() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
 		simulador.cadastroRendimento("Salario", 0f);
 		assertEquals(10000f, simulador.getTotalRendimento(), 0f);
 	}
 	
 	@Test (expected = ValorRendimentoInvalidoException.class)
-	public void testeCadastraRendimentoValorNegativo() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
+	public void testeCadastroRendimentoValorNegativo() throws DescricaoEmBrancoException, ValorRendimentoInvalidoException {
 		simulador.cadastroRendimento("Salario", -500f);
 		assertEquals(10000f, simulador.getTotalRendimento(), 0f);
 	}
