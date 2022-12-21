7833f0037818897ae2866962a8c1bb3e15ecd952
Denniel William <dennielwilliam.unb@gmail.com>
2022-12-20 21:55:51 -0300

    Aplicando triangulação no calculo total das deducoes

--- TP1/test/CalculoValorTotalDeducoes.java
+++ TP1/test/CalculoValorTotalDeducoes.java
@@ -1,38 +1,59 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.runners.Parameterized.*;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class CalculoValorTotalDeducoes {
     SimuladorIRPF simul;
+
+    Object[][] deducoes;
+    float valorEsperado;
 
     @Before
     public void setup(){
         simul = new SimuladorIRPF();
     }
 
-    @Test
-    public void ValorTotalUmaDeducao() throws ValorDeducaoInvalidoException, DescricaoEmBrancoException {
-        simul.cadastrarDeducao("Saúde", 450f);
+    public CalculoValorTotalDeducoes(Object[][] deducoes, float valorEsperado){
+        this.deducoes = deducoes;
+        this.valorEsperado = valorEsperado;
+    }
 
-        assertEquals(450f, simul.getTotalDeducoes(), 0f);
+    @Parameters
+    public static Collection<Object []> getParameters(){
+        return Arrays.asList(
+                new Object[][]{
+                        {new Object[][]{
+                                {"Saude", 2450f}
+                        }, 2450f},
+                        {new Object[][]{
+                                {"Saúde", 2450f},
+                                {"Educacao", 1090f}
+                        }, 3540f},
+                        {new Object[][]{
+                                {"Pensao", 1700f},
+                                {"Saúde", 2450f},
+                                {"Educacao", 1090f}
+                        }, 5240f},
+                }
+        );
     }
 
     @Test
-    public void ValorTotalDuasDeducoes() throws ValorDeducaoInvalidoException, DescricaoEmBrancoException {
-        simul.cadastrarDeducao("Saúde", 2450f);
-        simul.cadastrarDeducao("Educacao", 1090f);
+    public void testCalculoValorTotalDeducoes() throws ValorDeducaoInvalidoException, DescricaoEmBrancoException {
+        for(Object[] deducao: deducoes){
+            simul.cadastrarDeducao((String) deducao[0], (float) deducao[1]);
+        }
+        assertEquals(valorEsperado, simul.getTotalDeducoes(), 0f);
 
-
-        assertEquals(3540f, simul.getTotalDeducoes(), 0f);
-    }
-
-    @Test
-    public void ValorTotalTresDeducoes() throws ValorDeducaoInvalidoException, DescricaoEmBrancoException {
-        simul.cadastrarDeducao("Saúde", 2450f);
-        simul.cadastrarDeducao("Educacao", 1090f);
-        simul.cadastrarDeducao("Pensao", 1700f);
-
-        assertEquals(5240f, simul.getTotalDeducoes(), 0f);
     }
 
 }
