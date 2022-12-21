1d99d765553b777e0c325a990f4df53429744b20
Denniel William <dennielwilliam.unb@gmail.com>
2022-12-20 23:09:41 -0300

    Aplicando triangulação no calculo valor imposto

--- TP1/test/CalculoValorImposto.java
+++ TP1/test/CalculoValorImposto.java
@@ -1,8 +1,21 @@
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class CalculoValorImposto {
+    Object[][] rendimentos;
+    Object[][] deducoes;
+    int faixa;
+    String valorEsperado;
+
     private SimuladorIRPF simul;
 
     @Before
@@ -10,25 +23,44 @@ public class CalculoValorImposto {
         simul = new SimuladorIRPF();
     }
 
-    @Test
-    public void testCalculoValorImpostoFaixaUm() throws ValorRendimentoInvalidoException, DescricaoEmBrancoException, ValorDeducaoInvalidoException {
-        simul.cadastroRendimento("Salario", 5250f);
-        simul.cadastrarDeducao("Saude", 250f);
-
-        assertEquals("0,00", String.format("%.2f", simul.calcularValorImpostoFaixa(1)));
+    public CalculoValorImposto(Object[][] rendimentos, Object[][] deducoes, int faixa, String valorEsperado){
+        this.rendimentos = rendimentos;
+        this.deducoes = deducoes;
+        this.faixa = faixa;
+        this.valorEsperado = valorEsperado;
     }
-    @Test
-    public void testCalculoValorImpostoFaixaDois() throws ValorRendimentoInvalidoException, DescricaoEmBrancoException, ValorDeducaoInvalidoException {
-        simul.cadastroRendimento("Salario", 5250f);
-        simul.cadastrarDeducao("Saude", 250f);
 
-        assertEquals("69,20", String.format("%.2f", simul.calcularValorImpostoFaixa(2)));
+    @Parameters
+    public static Collection<Object[]> getParameters(){
+        return Arrays.asList(new Object[][]{
+                {new Object[][]{
+                        {"Salario", 5250f}
+                }, new Object[][]{
+                        {"Saude", 250f}
+                }, 1,"0,00"},
+                {new Object[][]{
+                        {"Salario", 5250f}
+                }, new Object[][]{
+                        {"Saude", 250f}
+                }, 2,"69,20"},
+                {new Object[][]{
+                        {"Salario", 5250f}
+                }, new Object[][]{
+                        {"Saude", 250f}
+                }, 3,"138,66"}
+        });
     }
-    @Test
-    public void testCalculoValorImpostoFaixaTres() throws ValorRendimentoInvalidoException, DescricaoEmBrancoException, ValorDeducaoInvalidoException {
-        simul.cadastroRendimento("Salario", 5250f);
-        simul.cadastrarDeducao("Saude", 250f);
 
-        assertEquals("138,66", String.format("%.2f", simul.calcularValorImpostoFaixa(3)));
+    @Test
+    public void testCalculoImpostoFaixa() throws ValorRendimentoInvalidoException, DescricaoEmBrancoException, ValorDeducaoInvalidoException {
+        for(Object [] rd: rendimentos){
+            simul.cadastroRendimento((String) rd[0], (Float) rd[1]);
+        }
+
+        for(Object [] de: deducoes){
+            simul.cadastrarDeducao((String) de[0], (Float) de[1]);
+        }
+
+        assertEquals(valorEsperado, String.format("%.2f", simul.calcularValorImpostoFaixa(faixa)));
     }
 }
